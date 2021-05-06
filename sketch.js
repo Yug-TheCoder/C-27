242b92fb63a87eda186271edd829d58dfadc3b23
Yug-TheCoder <yugsolankisingh@gmail.com>
2021-05-06 12:16:23 +0300

    sk

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var ConstrainedLog1, chain1;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -33,8 +35,13 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
+    ConstrainedLog1= new Log(230, 180, 80, PI/2);
 
     bird = new Bird(100,100);
+    chain= new Chain(bird.body,ConstrainedLog1.body);
+  
+  
+  
 
 }
 
@@ -61,4 +68,7 @@ function draw(){
 
     bird.display();
     platform.display();
+    ConstrainedLog1.display();
+    chain.display();
+
 }
